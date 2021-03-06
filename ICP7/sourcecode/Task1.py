c56af31c179edef3db13d94773fe884c904aa1b3
narhirep <77944933+narhirep@users.noreply.github.com>
2021-03-05 21:52:28 -0600

    Add files via upload

---
+++ ICP7/sourcecode/Task1.py
@@ -0,0 +1,53 @@
+# Importing libraries
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+tfidf_Vect = TfidfVectorizer()
+X_train_tfidf = tfidf_Vect.fit_transform(twenty_train.data)
+clf = KNeighborsClassifier(n_neighbors=3)
+from sklearn.naive_bayes import MultinomialNB
+
+clf = MultinomialNB()
+clf.fit(X_train_tfidf, twenty_train.target)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+X_test_tfidf = tfidf_Vect.transform(twenty_test.data)
+predicted = clf.predict(X_test_tfidf)
+score = metrics.accuracy_score(twenty_test.target, predicted)
+print(score)
+
+# Changing TF_IDF Vectorizer to use bi-grams and compare the accuracy
+
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+tfidf_Vect = TfidfVectorizer(ngram_range=(1, 2))
+X_train_tfidf = tfidf_Vect.fit_transform(twenty_train.data)
+
+# print(tfidf_Vect.vocabulary_)
+
+clf = MultinomialNB()
+clf.fit(X_train_tfidf, twenty_train.target)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+X_test_tfidf = tfidf_Vect.transform(twenty_test.data)
+predicted = clf.predict(X_test_tfidf)
+score = metrics.accuracy_score(twenty_test.target, predicted)
+print("Changing TF_IDF Vectorizer to use bi-grams and compare the accuracy")
+print(score)
+
+# Removing stop words and then comparing the accuracy
+
+print("Removing stop words and then comparing the accuracy")
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+tfidf_Vect = TfidfVectorizer(stop_words='english')
+X_train_tfidf = tfidf_Vect.fit_transform(twenty_train.data)
+
+# print(tfidf_Vect.vocabulary_)
+
+clf = MultinomialNB()
+clf.fit(X_train_tfidf, twenty_train.target)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
+X_test_tfidf = tfidf_Vect.transform(twenty_test.data)
+predicted = clf.predict(X_test_tfidf)
+score = metrics.accuracy_score(twenty_test.target, predicted)
+print(score)
